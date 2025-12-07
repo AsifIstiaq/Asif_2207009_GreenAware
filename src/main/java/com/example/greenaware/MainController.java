2bd8db6a2007e57260f557c9b97235218b595417
Asif Istiaque <asifistiaq47@gmail.com>
2025-12-08 02:09:09 +0600

    Navigation layout and MainController setup added

---
+++ src/main/java/com/example/greenaware/MainController.java
@@ -0,0 +1,41 @@
+package com.example.greenaware;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    public void showDashboard() {
+    }
+
+    @FXML
+    public void showIncidents() {
+    }
+
+    @FXML
+    public void showCategories() {
+    }
+
+    @FXML
+    public void showActions() {
+    }
+
+    @FXML
+    public void showWasteLogs() {
+    }
+
+    @FXML
+    public void showEnvironment() {
+    }
+
+    @FXML
+    public void showEvents() {
+    }
+
+    @FXML
+    public void showExportImport() {
+    }
+}
